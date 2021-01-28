2f15eed1603543edb693aea4102ef0b6853ad657
Gus Narea <gnarea@users.noreply.github.com>
2021-01-28 23:02:52 +0000

    chore: Remove reference to non-existing jest plugin

--- jest.config.js
+++ jest.config.js
@@ -117,7 +117,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["jest-date-mock"],
+  setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ["jest-extended"],
